d8d186c02c1d19f9d08239be40d8e89b818b9140
sairamn542 <sairamn542@gmail.com>
2024-11-01 11:46:39 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/jquery/dist/jquery.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+
+import Header from './components/Header';
+import Footer from './components/Footer.jsx';
+import Section from './components/Section.jsx';
+import Section2 from './components/Section2.jsx';
+import Management from './components/management.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ContactHeader from './Contact/ContactHeader.jsx';
+import ContactSection from './Contact/ContactSection.jsx';
+import ContactMap from './Contact/ContactMap.jsx';
+import Development from './Development/Development.jsx';
+import { Carousel } from '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import CarouselSection from './components/Carousel.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={
+          <>
+            <CarouselSection />
+            <Section />
+            <Section2 />
+            <Management />
+          </>
+        } />
+        <Route path='/contact' element={
+          <>
+            <ContactHeader />
+            <ContactSection />
+            <ContactMap />
+          </>
+        } />
+        <Route path='/development' element={
+          <>
+            <Development />
+          </>
+        } />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
